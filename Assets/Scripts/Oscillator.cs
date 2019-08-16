01333648a47038b08a526c839bbb0c08c1344ad9
JACPro <jamesacpro@gmail.com>
2019-08-16 15:14:27 +0100

    Auto-moving object, organise files

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    //todo remvove from inspector later
+
+    float movementFactor; //0 for not moved, 1 for fully moved
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; }
+
+        //todo protect against period = 0
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2; //roughly 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); //goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startPos + offset;
+    }
+}
